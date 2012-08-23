9ba84304d4aa95c59dbeebe0f331683388ec498c
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-08-23 16:11:32 +0530

    initial commit

---
+++ lib/main.rb
@@ -0,0 +1,19 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'rubygems'
+require 'mechanize'
+
+a = Mechanize.new { |agent|
+  agent.user_agent_alias = 'Mac Safari'
+}
+
+a.get('http://google.com/') do |page|
+  search_result = page.form_with(:name => 'f') do |search|
+    search.q = 'Hello world'
+  end.submit
+
+  search_result.links.each do |link|
+    puts link.text
+  end
+end
